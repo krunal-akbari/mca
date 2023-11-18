d8ae4a0de0d67665fd0b49bf9033becf628576eb
krunal <krunalakbari233@gmail.com>
2023-11-18 10:01:30 +0530

    this is made to be smoth

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6
 curl https://repo.anaconda.com/archive/Anaconda3-2021.11-Linux-x86_64.sh --output anaconda.sh
 bash anaconda.sh
+source ~/.bashrc
 conda env create --name first --file=test.yml
+pip install --upgrade pip \
+            pip install matplotlib \
+            seaborn\
+            scikit-learn \
+            tensorflow \
+            opencv-python
+
